43f75ea8d300cebcae3dcaeaeedb7ac4e824f8c5
Saylee Parab <saylee.parab@gatech.edu>
2017-11-21 17:20:36 -0500

    getting a fresh start

--- cs6250_server/server/Server.java
+++ cs6250_server/server/Server.java
@@ -1,20 +1,25 @@
 package server;
 //import SendFileServer.SendFile;
 
+import java.io.IOException;
 import java.net.*;
 
+import org.apache.avro.ipc.DatagramServer;
 import org.apache.avro.ipc.NettyServer;
 import org.apache.avro.ipc.specific.SpecificResponder;
 
 public class Server {
 	private static NettyServer server;
+	//private static DatagramServer server;
 	final static int port = 20001;
 	
-	public void InitServer() throws UnknownHostException {
+	public void InitServer() throws Exception {
 		InetSocketAddress socketAddr = new InetSocketAddress(port);
 		while (true) {
 			server = new NettyServer(new SpecificResponder(IBenchProtocol.class,
 					new BenchProtocolImpl()), socketAddr);
+			//server = new DatagramServer(new SpecificResponder(IBenchProtocol.class,
+				//	new BenchProtocolImpl()), socketAddr);
 			
 			try {
 				server.getPort();
@@ -25,11 +30,12 @@ public class Server {
 			break;
 		}
 		
-		System.err.println("Server is ready to accept connections");		
+		System.out.println("Server is ready to accept connections");		
 	}
 	
-	public void StartServer() {
+	public void StartServer() throws Exception {
 		server.start();
+		server.join();
 	}
 	
 	public static void main(String[] args) throws Exception {
