1104abfd34e02a04b9c30c501443b5f4a3dd4ad8
Saylee Parab <saylee.parab@gatech.edu>
2017-11-29 13:48:03 -0500

    UdpServer discard

--- cs6250_server/server/UdpServer.java
+++ cs6250_server/server/UdpServer.java
@@ -1,67 +1,36 @@
 package server;
-
-import java.net.InetAddress;
+import java.io.IOException;
 import java.net.InetSocketAddress;
-import java.net.SocketAddress;
-import java.net.UnknownHostException;
-import java.util.concurrent.Executors;
 
-import org.jboss.netty.bootstrap.ConnectionlessBootstrap;
-import org.jboss.netty.buffer.ChannelBuffer;
-import org.jboss.netty.buffer.ChannelBuffers;
-import org.jboss.netty.channel.Channel;
-import org.jboss.netty.channel.ChannelHandlerContext;
-import org.jboss.netty.channel.ChannelPipeline;
-import org.jboss.netty.channel.ChannelPipelineFactory;
-import org.jboss.netty.channel.Channels;
-import org.jboss.netty.channel.MessageEvent;
-import org.jboss.netty.channel.SimpleChannelHandler;
-import org.jboss.netty.channel.socket.nio.NioDatagramChannelFactory;
+import org.apache.avro.ipc.DatagramServer;
+import org.apache.avro.ipc.specific.SpecificResponder;
 
-public class UdpServer {
+import cs6250.benchmarkingsuite.imageprocessing.server.IBenchProtocol;
 
-    int port=20001;
-    private Channel serverChannel;
+public class UDPServer {
 
-    public UdpServer( int port ) {
-        super();
-        this.port = port;
-    }
+	private DatagramServer server;
+	final static int port = 30001;
+	public void InitServer() throws IOException {
+		InetSocketAddress socketAddr = new InetSocketAddress(port);
+		while (true) {
+			server = new DatagramServer(new SpecificResponder(IBenchProtocol.class,
+					new BenchProtocolImpl()), socketAddr);
+			
+			try {
+				server.getPort();
+			} catch (NullPointerException e) {
+				e.printStackTrace();
+			}
+			
+			break;
+		}
+		
+		System.err.println("Server is ready to accept connections");		
+	}
+	
 
-    public int getport() {
-    	return this.port;
-    }
-    public void start() {
-        NioDatagramChannelFactory serverChannelFactory =
-            new NioDatagramChannelFactory( Executors.newCachedThreadPool(), 1 );
-        ConnectionlessBootstrap serverBootstrap =
-            new ConnectionlessBootstrap( serverChannelFactory );
-        serverBootstrap.setPipelineFactory( new ChannelPipelineFactory() {
-            @Override
-            public ChannelPipeline getPipeline() {
-                return Channels.pipeline( new MyChannelHandler() {
-                    @Override
-                    public void messageReceived( ChannelHandlerContext ctx,
-                        MessageEvent e ) {
-                        // TODO, handle message from client
-                    	
-                    }
-                } );
-            }
-        } );
-        serverBootstrap.setOption( "reusePort", Boolean.TRUE );
-        final InetSocketAddress trafficAddress = new InetSocketAddress( port );
-        serverChannel = serverBootstrap.bind( trafficAddress );
-    }
-
-    
-    public void sendMessage( byte[] message, String clientIp )
-        throws UnknownHostException {
-        // TODO, how do I control the source port of this packet??
-        SocketAddress address =
-            new InetSocketAddress( InetAddress.getByName( clientIp ), port );
-        ChannelBuffer buffer = ChannelBuffers.wrappedBuffer( message );
-        serverChannel.write( buffer, address );
-    }
-
+	public void StartServer() {
+		server.start();
+	}
 }
